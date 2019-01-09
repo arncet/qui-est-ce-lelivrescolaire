83ee90a9eaf2dc4fff3051e7c1b56f936bc1f7c1
arncet <arnaud.cetoute@hotmail.fr>
2019-01-09 15:27:40 +0100

    Updates

---
+++ precache-manifest.54e8d7b41eb75d4e3c976bb53c07d9b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2b6bbb799302ddca88c",
+    "url": "/qui-est-ce-lelivrescolaire/static/js/runtime~main.e2b6bbb7.js"
+  },
+  {
+    "revision": "4e6e9320544cbecd7a83",
+    "url": "/qui-est-ce-lelivrescolaire/static/js/main.4e6e9320.chunk.js"
+  },
+  {
+    "revision": "a86bf717a01ada85b860",
+    "url": "/qui-est-ce-lelivrescolaire/static/js/1.a86bf717.chunk.js"
+  },
+  {
+    "revision": "94b45f5cd0eabf893c8345b48b278ebc",
+    "url": "/qui-est-ce-lelivrescolaire/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/qui-est-ce-lelivrescolaire/precache-manifest.25848d56651f7fb1f67e22fc7fd0536a.js"
+  "/qui-est-ce-lelivrescolaire/precache-manifest.54e8d7b41eb75d4e3c976bb53c07d9b2.js"
 );
 
 workbox.clientsClaim();
