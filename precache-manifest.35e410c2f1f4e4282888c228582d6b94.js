e2170d4d7639fa26da024624d4f152a44b6a1428
arncet <arnaud.cetoute@hotmail.fr>
2019-01-11 12:48:37 +0100

    Updates

--- precache-manifest.35e410c2f1f4e4282888c228582d6b94.js
+++ precache-manifest.35e410c2f1f4e4282888c228582d6b94.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/qui-est-ce-lelivrescolaire/static/js/runtime~main.e2b6bbb7.js"
   },
   {
-    "revision": "829d0be429c2a7ac370b",
-    "url": "/qui-est-ce-lelivrescolaire/static/js/main.829d0be4.chunk.js"
+    "revision": "a433a0c5d2005a46293b",
+    "url": "/qui-est-ce-lelivrescolaire/static/js/main.a433a0c5.chunk.js"
   },
   {
     "revision": "6ad5ca9428eef6d1abfd",
     "url": "/qui-est-ce-lelivrescolaire/static/js/1.6ad5ca94.chunk.js"
   },
   {
-    "revision": "1830f0290c341b57a883376a42026f64",
+    "revision": "21ac6622dc52b26b8be40a99890e7c9d",
     "url": "/qui-est-ce-lelivrescolaire/index.html"
   }
 ];
