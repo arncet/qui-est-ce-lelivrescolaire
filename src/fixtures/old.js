8d269fb4a93535210d0c98d026403e7e86fe7b9d
Arnaud CETOUTE <arnaud.cetoute@hotmail.fr>
2019-10-24 16:59:00 +0200

    update

--- src/fixtures/old.js
+++ src/fixtures/old.js
@@ -26,4 +26,5 @@ export default [
   { id: 36, name: 'Clementine Gauthier', picture: 'images/pictures/old/clementine.jpg' },
   { id: 67, name: 'Eleonore Peres', picture: 'images/pictures/old/eleonore.jpg' },
   { id: 43, name: 'Elisabeth Frouart', picture: 'images/pictures/old/elisabeth.jpg' },
+  { id: 43, name: 'Vincent Bourgeois', picture: 'images/pictures/old/vincent.jpg' },
 ]
